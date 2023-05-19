5fb9f34f0c2b7fb8d23a6d0ad5d8430129e54303
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-19 06:04:22 -0700

    Executes a command

---
+++ execute.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * execute - executes a command with given arguments
+ *
+ * @array: array of command and arguments
+ */
+
+void execute(char **array)
+{
+	pid_t child_pid;
+	int ex_status, status;
+
+	child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error");
+			exit(1);
+		}
+		if (child_pid == 0)
+		{
+			ex_status = execve(array[0], array, environ);
+			if (ex_status == -1)
+			{
+				perror(array[0]);
+				exit(1);
+			}
+		}
+		wait(&status);
+
+}
