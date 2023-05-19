40f185e6682fa5684eb3ce34d7ad040ead725348
GirumZ <girum.zebe@gmail.com>
2023-05-19 02:10:19 -0700

    Header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MANE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+
+#endif /* MAIN_H */
