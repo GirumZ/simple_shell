e5a86f1cc428621046678e482aba028a97a508ba
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-19 02:21:58 -0700

    Main function

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @av: vector of arguments
+ * @ac: vector arguments
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char *av[])
+{
+	(void) ac;
+	(void) av;
+
+	shell_loop();
+	return (0);
+}
