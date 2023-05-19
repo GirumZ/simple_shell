868b805164c3f20b4e1c4a78d9e668a39d0fad85
GirumZ <girum.zebe@gmail.com>
2023-05-19 02:12:03 -0700

    String compare

---
+++ strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares to strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: the difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, diff;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+		diff = s1[i] - s2[i];
+		if (diff != 0)
+			break;
+	}
+	return (diff);
+}
