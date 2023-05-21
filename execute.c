fcba89b8a890553f7118d601126cbd2170dd64ec
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-21 12:14:16 -0700

    Excute function

--- execute.c
+++ execute.c
@@ -8,24 +8,24 @@
 
 void execute(char **array)
 {
-	pid_t child_pid;
-	int ex_status, status;
+        pid_t child_pid;
+        int ex_status;
 
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error");
-		exit(1);
-	}
-	if (child_pid == 0)
-	{
-		ex_status = execve(array[0], array, environ);
-		if (ex_status == -1)
-		{
-			perror(array[0]);
-			exit(1);
-		}
-	}
-	wait(&status);
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+                perror("Error");
+                exit(1);
+        }
+        if (child_pid == 0)
+        {
+                ex_status = execve(array[0], array, environ);
+                if (ex_status == -1)
+                {
+                        perror(array[0]);
+                        exit(1);
+                }
+        }
+        wait(NULL);
 
 }
