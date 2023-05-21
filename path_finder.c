c9388ea89bb84be928d2849648d6eb06699b1df9
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-21 12:19:50 -0700

    path finder

--- path_finder.c
+++ path_finder.c
@@ -1,35 +1,39 @@
 #include "main.h"
+
 /**
  * true_path - checks if a path is found for a command
  * @cmd: the command
- * Return: pointer to the full_path is found and NULL if not
+ * Return: pointer to the full_path if found, and NULL if not
  */
-
-
-/*list_t *path_list(void)*/
 char *true_path(char *cmd)
 {
-	char *name = "PATH";
-	char *path = NULL, *full_path = NULL;
-	char **paths = NULL;
-	int i, size = 0;
+        char *name = "PATH";
+        char *path = NULL, *full_path = NULL;
+        char **paths = NULL;
+        int i, size = 0;
 
+        path = _getenv(name);
+        paths = tokenizer(path, ":");
 
-	path = _getenv(name);
-	paths = tokenizer(path, ":");
+        for (i = 0; paths[i] != NULL; i++)
+        {
+                size = strlen(paths[i]) + strlen(cmd) + 2;
+                full_path = malloc(sizeof(char) * size);
+                if (full_path == NULL)
+                {
+                        perror("Error allocating memory");
+                        exit(1);
+                }
 
-	for (i = 0 ; paths[i] != NULL ; i++)
-	{
-		size = (strlen(paths[i]) + strlen(cmd) + 2);
-		full_path = malloc(sizeof(char) * size);
-		strcpy(full_path, paths[i]);
-		strcat(full_path, "/");
-		strcat(full_path, cmd);
-		full_path[size - 1] = '\0';
+                strcpy(full_path, paths[i]);
+                strcat(full_path, "/");
+                strcat(full_path, cmd);
+                full_path[size - 1] = '\0';
 
-		if (access(full_path, X_OK) == 0)
-			return (full_path);
-		free(full_path);
-	}
-	return (NULL);
+                if (access(full_path, X_OK) == 0)
+                        return full_path;
+
+                free(full_path);
+        }
+	 return (NULL);
 }
