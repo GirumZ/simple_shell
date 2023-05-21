3fca9d97b66d841c577c83efb5f7228f599f65e6
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-21 12:15:07 -0700

    Free list function

--- free_list.c
+++ free_list.c
@@ -7,13 +7,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *current;
+        list_t *current;
 
-	while (head)
-	{
-		current = head->next;
-		free(head->dir);
-		free(head);
-		head = current;
-	}
+        while (head)
+        {
+                current = head->next;
+                free(head->dir);
+                free(head);
+                head = current;
+        }
 }
