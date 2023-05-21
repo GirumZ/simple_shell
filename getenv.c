947f07d4703235c6a3edcfa40113fbe879718e75
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-05-21 12:17:16 -0700

    Get environment

--- getenv.c
+++ getenv.c
@@ -7,13 +7,13 @@
 
 char *_getenv(char *var_name)
 {
-	char *value = NULL;
-	int i, n = _strlen(var_name);
+        char *value = NULL;
+        int i, n = _strlen(var_name);
 
-	for (i = 0 ; environ[i] != NULL ; i++)
-	{
-		if (_strncmp(var_name, environ[i], n) == 0)
-			value = environ[i] + n + 1;
-	}
-	return (value);
+        for (i = 0 ; environ[i] != NULL ; i++)
+        {
+                if (_strncmp(var_name, environ[i], n) == 0)
+                        value = environ[i] + n + 1;
+        }
+        return (value);
 }
