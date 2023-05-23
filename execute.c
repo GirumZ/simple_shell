075cd368bedee28626c7db0704acd0aadbe3fb8b
GirumZ <girum.zebe@gmail.com>
2023-05-23 04:35:48 -0700

    execute function

---
+++ execute.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * execute - executes a command with given arguments
+ * @cmd: the command
+ * @array: array of command and arguments
+ */
+
+void execute(char *cmd, char **array)
+{
+	char **env = environ;
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		exit(1);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(cmd, array, env) == -1)
+			perror(cmd);
+	}
+	else
+	{
+		wait(&status);
+		free(array);
+	}
+}
