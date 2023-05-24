02089f48e42888c8e248b921535c33e959ac7fde
GirumZ <girum.zebe@gmail.com>
2023-05-24 06:13:54 -0700

    command handler

--- command_handler.c
+++ command_handler.c
@@ -2,10 +2,12 @@
 /**
  * cmd_handle - handles the diffferent types of commands
  * @array: array of command and arguments
+ * @av: vector of arguments for main
+ *
  * Return: 0 on success and 1 on failure
  */
 
-int cmd_handle(char **array)
+int cmd_handle(char **array, char **av)
 {
 	struct stat st;
 	char *full_cmd;
@@ -30,7 +32,11 @@ int cmd_handle(char **array)
 		iterate_free(array);
 		return (0);
 	}
-	perror(array[0]);
+	write(STDERR_FILENO, av[0], _strlen(av[0]));
+	write(STDERR_FILENO, ": 1: ", 5);
+	write(STDERR_FILENO, array[0], _strlen(array[0]));
+	write(STDERR_FILENO, ": not found\n", 12);
+	/*perror(array[0]);*/
 	free(full_cmd);
 	iterate_free(array);
 	return (0);
